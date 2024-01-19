4630b5ee5ed9512778a04e513101e28bdc0443f0
Mukhamet1 <muhaerinbek123@gmail.com>
2024-01-19 20:19:54 +0600

    hom word 4

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let array = [1,24,3,4,57,6,72,8,979]
+Math.max(...array)
+console.log(Math.max(...array))
+
+let array2 = [168,2,3,34,5,686,757,8,9]
+Math.min(...array2)
+console.log(Math.min(...array2));
